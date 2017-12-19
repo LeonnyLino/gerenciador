f67a6aa70708fdf1bf045668e62450e11abfd268
LeonnyLino <leonny.1993@gmail.com>
2017-12-19 01:57:18 -0200

    -Foi criada a classe login, e modicado o index para fazer o formulario de login. -classe de filtro foi implementada para registar o cookie junto do login.

--- src/main/java/br/com/alura/gerenciador/web/FiltroDeAuditoria.java
+++ src/main/java/br/com/alura/gerenciador/web/FiltroDeAuditoria.java
@@ -1,6 +1,7 @@
 package br.com.alura.gerenciador.web;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -9,7 +10,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
-import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 @WebFilter(urlPatterns="/*")
@@ -24,11 +25,34 @@ public class FiltroDeAuditoria implements Filter {
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
+		
 		HttpServletRequest req = (HttpServletRequest) request;
-		System.out.println("O LeonnyLino estar acessando a URI: "+ req.getRequestURI());
+		String usuario = "<deslogado>";
+		Cookie cookie = getUsuario(req);
+		
+		if(cookie != null) 
+			usuario = cookie.getValue();
+	
+		System.out.println("O LeonnyLino estar acessando com usuario: "+usuario+" na URI: "+req.getRequestURI());
 		
 		chain.doFilter(request, response);
 		
+	}
+	
+	private Cookie getUsuario (HttpServletRequest req) {
+		Cookie[] cookies = req.getCookies();
+		
+		if(cookies == null) {
+			return null;
+		}
+		
+		for(Cookie cookie : cookies) {
+			if(cookie.getName().equals("usuario.logado")) {
+				return cookie;
+			}
+		}
+		
+		return null;
 	}
 
 	@Override
